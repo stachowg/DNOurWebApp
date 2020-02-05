126ed8b06d3c4b13b68c3d3e5103ede6df6d723b
Grzegorz Stachowicz <gs@provolution.pl>
2020-02-05 09:50:30 +0100

    first commit

---
+++ OurWebApp.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿namespace OurWebApp.Web.Pages
+{
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.Extensions.Configuration;
+
+    public class IndexModel : PageModel
+    {
+        private readonly IConfiguration Config;
+
+        // Constructor that that takes IConfiguration is called on instantiation thanks to Dependency injection
+        public IndexModel(IConfiguration config)
+        {
+            Config = config;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ OurWebApp.Web/Startup.cs
@@ -0,0 +1,50 @@
+namespace OurWebApp.Web
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddSingleton(Configuration);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+            if (env.IsDevelopment())
+            {
+                // app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc();
+        }
+    }
+}
